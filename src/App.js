b3283e0c3b8d54fe15d2d92fb90db3f38f41be71
Khayrul Islam <khayrulislam2525@gmail.com>
2022-05-08 10:20:24 +0600

    redux setup complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decrement, increment } from './redux/spaceXDataSlice';
 
 function App() {
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch()
+
   return (
     <div>
-      <h1 className='text-red-500'>Hello world</h1>
+      <h1>Counter No:{count} </h1>
+      <button onClick={() => dispatch(increment())}>Increment By 1</button>
+      <button onClick={() => dispatch(decrement())}>Decrement By 1</button>
     </div>
   );
 }
